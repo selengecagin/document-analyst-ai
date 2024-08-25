721138df87e9b15d3b1fcc310387b447b02fb0c5
selengecagin <selengecagin@gmail.com>
2024-08-25 20:43:00 +0300

    Update dependencies

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
