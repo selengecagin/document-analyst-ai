b6d0f2fe3a24333760f2bbd6284703bf5f41e357
selengecagin <selengecagin@gmail.com>
2024-08-25 17:13:14 +0300

    Fix typos and variable consistency

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from gitdb.fun import chunk_size
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import  ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievelChain
+from langchain.chains import ConversationalRetrievalChain
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -29,16 +28,16 @@ def get_text_chunks(text):
 
 def get_vector_store(text_chunks):
     embeddings = OpenAIEmbeddings()
-    vector_store = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
+    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vector_store
 
-def get_conversation_chain(vectorstore):
+def get_conversation_chain(vector_store):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key='chat_history',return_message=True)
-    conversation_chain = ConversationalRetrievelChain.from_llm(
-        llm = llm,
-        retriever = vectorstore.as_retriever(),
-        memory = memory
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
     )
     return conversation_chain
 
@@ -49,28 +48,16 @@ def main():
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
 
-    # Sidebar to users upload the PDFs, with for put things inside the sidebar
     with st.sidebar:
         st.subheader("Your Documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", type="pdf", accept_multiple_files=True)
         if st.button("Process"):
            with st.spinner("Processing your documents..."):
-
-            # get the pdf text - will return a single string of text with all given content
             raw_text = get_pdf_text(pdf_docs)
-            # st.write(raw_text)
-
-            # get the text chunks
             text_chunks = get_text_chunks(raw_text)
-            # st.write(text_chunks)
-
-            # create vector store
             vector_store = get_vector_store(text_chunks)
-
-            # create conversation chain
-            conversation = get_conversation_chain(vectorstore)
-
+            conversation = get_conversation_chain(vector_store)
 
 if __name__ == '__main__':
     main()
