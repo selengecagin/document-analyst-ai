526d84528bed21eca6071709def13e7ea2ad41f8
selengecagin <selengecagin@gmail.com>
2024-08-25 14:13:43 +0300

    Add sidebar for PDF uploads and processing

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ def main():
    st.header("Chat with multiple PDFs :books:")
    st.text_input("Ask a question about your documents:")
 
+   # Sidebar to users upload the PDFs, with for put things inside the sidebar
+   with st.sidebar:
+       st.subheader("Your Documents")
+       st.file_uploader("Upload your PDFs here and click on 'Process'", type="pdf", accept_multiple_files=True)
+       st.button("Process")
+
 
 if __name__ == '__main__':
     main()
