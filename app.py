4ab1bcdff442899e61e971839ff2089c48a094a7
selengecagin <selengecagin@gmail.com>
2024-08-25 14:26:10 +0300

    Load environment variables

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import streamlit as st
-
-
+from dotenv import load_dotenv
 
 def main():
-   st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:", layout="wide")
+    load_dotenv()
 
-   st.header("Chat with multiple PDFs :books:")
-   st.text_input("Ask a question about your documents:")
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:", layout="wide")
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
 
-   # Sidebar to users upload the PDFs, with for put things inside the sidebar
-   with st.sidebar:
-       st.subheader("Your Documents")
-       st.file_uploader("Upload your PDFs here and click on 'Process'", type="pdf", accept_multiple_files=True)
-       st.button("Process")
-
+    # Sidebar to users upload the PDFs, with for put things inside the sidebar
+    with st.sidebar:
+        st.subheader("Your Documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'", type="pdf", accept_multiple_files=True)
+        st.button("Process")
 
 if __name__ == '__main__':
     main()
