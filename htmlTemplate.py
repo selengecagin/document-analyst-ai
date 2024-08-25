be5fe83b5f468d2a74c20e4d8a99b5662b60aecb
selengecagin <selengecagin@gmail.com>
2024-08-25 17:36:30 +0300

    Create user message template for chat interface

--- htmlTemplate.py
+++ htmlTemplate.py
@@ -33,3 +33,12 @@ bot_template = '''
     <div class="message">{{MSG}}</div>
 </div>
 '''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
